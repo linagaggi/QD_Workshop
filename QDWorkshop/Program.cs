675e0775ba5e43716f9621d8674b5a0bea493480
LinaGaggi <l.gaggi@cogit.se>
2019-10-22 22:46:14 +0200

    created project

---
+++ QDWorkshop/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QDWorkshop
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number: ");
+            var x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number: ");
+            var y = Convert.ToInt32(Console.ReadLine());
+
+            var mathOperations = new MathOperations(x, y);
+            Console.WriteLine(mathOperations.Multiply());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Tests/MathOperationsTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QDWorkshop;
+
+namespace Tests
+{
+    [TestClass]
+    public class MathOperationsTest
+    {
+        [TestMethod]
+        public void ShouldMultiply2Integers()
+        {
+            //Arrange
+            var x = 2;
+            var y = 3;
+            var expected = "x * y = 6";
+            IMathOperations mathOperations = new MathOperations(x, y);
+
+            //Act
+            var result = mathOperations.Multiply();
+
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
