98b23e4f5d4d2ef1c790736c4b30f33599f887be
Isac Persson <isac.persson@qd.se>
2019-10-23 11:08:17 +0200

    Uppercase Function

---
+++ QDWorkshop/StringOperations.cs
@@ -0,0 +1,20 @@
+﻿
+namespace QDWorkshop
+{
+    public interface IStringOperations
+    {
+        string Uppercase(string str);
+    }
+    public class StringOperations : IStringOperations
+    {
+        public StringOperations()
+        {
+
+        }
+
+        public string Uppercase(string str)
+        {
+            return str.ToUpper();
+        }
+    }
+}
---
+++ Tests/StringOperationsTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QDWorkshop;
+
+namespace Tests
+{
+    [TestClass]
+    public class StringOperationsTest
+    {
+        [TestMethod]
+        public void ShouldMakeUppercase()
+        {
+            //Arrange
+            var str = "This is a Test String";
+            var expected = "THIS IS A TEST STRING";
+            IStringOperations stringOps = new StringOperations();
+            //Act
+            var result = stringOps.Uppercase(str);
+
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
